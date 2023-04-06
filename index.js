4423d945773f280a72fef522edce81bfab133413
Kennedy <kiplangatkennedy019@gmail.com>
2023-04-06 11:42:35 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, myDriver){
+    return drivers.filter((elements) => elements.toLowerCase() === myDriver.toLowerCase()
+    )
+};
+
+function fuzzyMatch(drivers, letters){
+    return drivers.filter(function(drivers){  
+      return drivers.toLowerCase().startsWith(letters.toLowerCase())
+    })
+  }
+  
+  function matchName(drivers, myDriver){
+    return drivers.filter((record) => record.name === myDriver)
+};
